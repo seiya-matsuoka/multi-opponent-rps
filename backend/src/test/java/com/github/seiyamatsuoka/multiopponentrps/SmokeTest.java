d940b20e2b266dd85e1643444e5e5665ee416f05
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-11 22:36:57 +0900

    test: SmokeTestを追加

---
+++ backend/src/test/java/com/github/seiyamatsuoka/multiopponentrps/SmokeTest.java
@@ -0,0 +1,14 @@
+package com.github.seiyamatsuoka.multiopponentrps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/** Spring の ApplicationContext が正常に起動できることを確認するためのスモークテスト。 */
+@SpringBootTest
+class SmokeTest {
+
+  @Test
+  void contextLoads() {
+    // ApplicationContext の起動に成功すればこのテストは通る。
+  }
+}
