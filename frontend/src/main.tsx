7cf3e236ce7a9f24ebcfc606bb9f6b978f741f8a
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-01-17 06:23:43 +0900

    feat: 開発時のAPI呼び出し口を main.tsx に追加

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
+import { registerApiDevtools } from './api/devtools';
 
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
@@ -12,6 +13,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+// render の前に1回だけ実行
+registerApiDevtools();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
